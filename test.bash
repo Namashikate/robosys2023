96c8ba64378be4ba6df358ae10438ae12a972f87
Namashikate <kaito160306@gmail.com>
2023-10-26 12:56:08 +0900

    Change a command

--- test.bash
+++ test.bash
@@ -18,7 +18,8 @@ out=$(echo あ | ./plus)
 [ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-
+[ "$?" = 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 [ "$res" = 0 ] && echo OK
 exit $res
 
